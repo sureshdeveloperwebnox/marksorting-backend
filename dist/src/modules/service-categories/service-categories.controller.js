5e3fbedd69f6e2fb3397ae028c39156ebd8a81c4
sureshdeveloperwebnox <sureshdeveloperwebnox@gmail.com>
2026-05-21 17:17:19 +0530

    feat: add the Service Category crud module

--- dist/src/modules/service-categories/service-categories.controller.js
+++ dist/src/modules/service-categories/service-categories.controller.js
@@ -8,18 +8,87 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ServiceCategoriesController = void 0;
 const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
 const service_categories_service_1 = require("./service-categories.service");
+const create_service_category_dto_1 = require("./dto/create-service-category.dto");
+const update_service_category_dto_1 = require("./dto/update-service-category.dto");
 let ServiceCategoriesController = class ServiceCategoriesController {
     serviceCategoriesService;
     constructor(serviceCategoriesService) {
         this.serviceCategoriesService = serviceCategoriesService;
     }
+    findAll(skip, take, search, status) {
+        return this.serviceCategoriesService.findAll({
+            skip: skip ? parseInt(skip, 10) : undefined,
+            take: take ? parseInt(take, 10) : undefined,
+            search,
+            status,
+        });
+    }
+    findOne(id) {
+        return this.serviceCategoriesService.findById(id);
+    }
+    create(dto) {
+        return this.serviceCategoriesService.create(dto);
+    }
+    update(id, dto) {
+        return this.serviceCategoriesService.update(id, dto);
+    }
+    remove(id) {
+        return this.serviceCategoriesService.remove(id);
+    }
 };
 exports.ServiceCategoriesController = ServiceCategoriesController;
+__decorate([
+    (0, common_1.Get)(),
+    (0, swagger_1.ApiOperation)({ summary: 'Get all service categories with pagination and filtering' }),
+    __param(0, (0, common_1.Query)('skip')),
+    __param(1, (0, common_1.Query)('take')),
+    __param(2, (0, common_1.Query)('search')),
+    __param(3, (0, common_1.Query)('status')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String, String, String]),
+    __metadata("design:returntype", void 0)
+], ServiceCategoriesController.prototype, "findAll", null);
+__decorate([
+    (0, common_1.Get)(':id'),
+    (0, swagger_1.ApiOperation)({ summary: 'Get service category by ID' }),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], ServiceCategoriesController.prototype, "findOne", null);
+__decorate([
+    (0, common_1.Post)(),
+    (0, swagger_1.ApiOperation)({ summary: 'Create new service category' }),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [create_service_category_dto_1.CreateServiceCategoryDto]),
+    __metadata("design:returntype", void 0)
+], ServiceCategoriesController.prototype, "create", null);
+__decorate([
+    (0, common_1.Put)(':id'),
+    (0, swagger_1.ApiOperation)({ summary: 'Update existing service category' }),
+    __param(0, (0, common_1.Param)('id')),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, update_service_category_dto_1.UpdateServiceCategoryDto]),
+    __metadata("design:returntype", void 0)
+], ServiceCategoriesController.prototype, "update", null);
+__decorate([
+    (0, common_1.Delete)(':id'),
+    (0, swagger_1.ApiOperation)({ summary: 'Soft delete service category' }),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], ServiceCategoriesController.prototype, "remove", null);
 exports.ServiceCategoriesController = ServiceCategoriesController = __decorate([
     (0, swagger_1.ApiTags)('service-categories'),
     (0, common_1.Controller)('service-categories'),
--- src/modules/service-categories/service-categories.controller.ts
+++ src/modules/service-categories/service-categories.controller.ts
@@ -1,9 +1,60 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Put,
+    Param,
+    Delete,
+    Query,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { ServiceCategoriesService } from './service-categories.service';
+import { CreateServiceCategoryDto } from './dto/create-service-category.dto';
+import { UpdateServiceCategoryDto } from './dto/update-service-category.dto';
 
 @ApiTags('service-categories')
 @Controller('service-categories')
 export class ServiceCategoriesController {
     constructor(private readonly serviceCategoriesService: ServiceCategoriesService) { }
+
+    @Get()
+    @ApiOperation({ summary: 'Get all service categories with pagination and filtering' })
+    findAll(
+        @Query('skip') skip?: string,
+        @Query('take') take?: string,
+        @Query('search') search?: string,
+        @Query('status') status?: string,
+    ) {
+        return this.serviceCategoriesService.findAll({
+            skip: skip ? parseInt(skip, 10) : undefined,
+            take: take ? parseInt(take, 10) : undefined,
+            search,
+            status,
+        });
+    }
+
+    @Get(':id')
+    @ApiOperation({ summary: 'Get service category by ID' })
+    findOne(@Param('id') id: string) {
+        return this.serviceCategoriesService.findById(id);
+    }
+
+    @Post()
+    @ApiOperation({ summary: 'Create new service category' })
+    create(@Body() dto: CreateServiceCategoryDto) {
+        return this.serviceCategoriesService.create(dto);
+    }
+
+    @Put(':id')
+    @ApiOperation({ summary: 'Update existing service category' })
+    update(@Param('id') id: string, @Body() dto: UpdateServiceCategoryDto) {
+        return this.serviceCategoriesService.update(id, dto);
+    }
+
+    @Delete(':id')
+    @ApiOperation({ summary: 'Soft delete service category' })
+    remove(@Param('id') id: string) {
+        return this.serviceCategoriesService.remove(id);
+    }
 }
